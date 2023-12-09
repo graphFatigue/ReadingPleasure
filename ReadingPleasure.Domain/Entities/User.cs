03f8057a8d5396ca0ce85c3cb76eac3b81f75ad5
graphFatigue <foxmary007@gmail.com>
2023-12-09 15:22:48 +0200

    initial commit

---
+++ ReadingPleasure.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingPleasure.Domain.Entities
+{
+    public class User : IdentityUser<Guid>, IBaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ ReadingPleasure.Infrastructure/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ReadingPleasure.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingPleasure.Infrastructure
+{
+    public class AppDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
